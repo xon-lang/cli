d8741f148f18dddf31a15ee1bdc3822d951413b3
nizami <nizami.dev@gmail.com>
2020-08-17 01:37:58 +0300

    Add simple test

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { parseCode, ProgramTree } from "@xon/ast";
+import { ProgramTranslator } from "@xon/translator-ts";
+import * as fs from "fs";
+import * as path from "path";
+
+function run(filename) {
+    if (!filename) return console.log("No input file");
+
+    const inputPath = path.resolve(process.cwd(), filename);
+    const code = fs.readFileSync(inputPath).toString();
+    const tree = parseCode(code, ProgramTree);
+    const translator = new ProgramTranslator(tree);
+    const outputPath = path.resolve(
+        path.dirname(inputPath),
+        path.basename(inputPath)
+    );
+    console.log(inputPath);
+    console.log(outputPath);
+    fs.writeFileSync(outputPath, translator.translate());
+}
+
+run(process.argv.slice(2)[0]);
