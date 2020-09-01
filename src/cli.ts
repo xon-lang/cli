ac513f6e8e5956d7cf0139e8a20b1f0c60071e6e
nizami <nizami.dev@gmail.com>
2020-09-01 19:18:50 +0300

    Fix code translation

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-import { parseCode, ProgramTree } from "@xon/ast";
-import { ProgramTranslator } from "@xon/translator-ts";
+import { translateProgram } from "@xon/translator-ts";
 import * as fs from "fs";
 import * as path from "path";
 
@@ -10,15 +9,14 @@ function compile(filename: string) {
 
     const inputPath = path.resolve(process.cwd(), filename);
     const code = fs.readFileSync(inputPath).toString();
-    const tree = parseCode(code, ProgramTree);
-    const translator = new ProgramTranslator(tree);
+    const outputCode = translateProgram(code);
     const outputPath = path.resolve(
         path.dirname(inputPath),
         path.basename(inputPath).split(".").slice(0, -1).join(".") + ".ts"
     );
     console.log(inputPath);
     console.log(outputPath);
-    fs.writeFileSync(outputPath, translator.translate());
+    fs.writeFileSync(outputPath, outputCode);
 }
 
 compile(process.argv.slice(2)[0]);
